a761e8f75ef656cb06edf8d6445fd4e70358acd1
Isabella <Bellagomesp2003@gmail.com>
2022-10-11 15:05:38 -0300

    Initial Commit

---
+++ AtividadeGit/AtividadeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtividadeGitApp.swift
+//  AtividadeGit
+//
+//  Created by Isabella Gomes  on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AtividadeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
