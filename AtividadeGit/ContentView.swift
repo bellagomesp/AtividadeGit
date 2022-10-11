90113369caf3c4472f15b2fd5d4cac8936f40b3a
Isabella <Bellagomesp2003@gmail.com>
2022-10-11 15:34:11 -0300

    mensagem é verdade

--- AtividadeGit/ContentView.swift
+++ AtividadeGit/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Amo a Vivian")
+            Text("É verdade")
         }
         .padding()
     }
